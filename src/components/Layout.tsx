ac1b01291737ece418871f180fe125bafa2140e0
joagz <joagomez.dev@gmail.com>
2024-02-11 11:09:37 -0300

    update changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import { JSX } from "solid-js"
 
-type Props = { children: JSX.Element[] | JSX.Element }
+type Props = {
+  children: JSX.Element[] | JSX.Element, class?: string;
+}
 
-export default function Layout({ children }: Props) {
+export default function Layout(props: Props) {
   return (
     <main class={`w-full flex flex-col items-center pt-32 pb-32`}>
-      <article class={`p-8 sm:p-0 flex flex-col gap-5 w-full sm:w-3/4 lg:w-1/2`}>
-        {children}
+      <article class={`p-8 sm:p-0 flex flex-col gap-5 w-full sm:w-3/4 lg:w-1/2  ${props.class}`}>
+        {props.children}
       </article>
     </main>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,33 @@
 import { A } from "@solidjs/router";
-import { createEffect, createSignal } from "solid-js";
-import { Transition } from "solid-transition-group";
 import Layout from "~/components/Layout";
 
 export default function Home() {
 
   return (
-    <Layout>
-      <span class="bg-white text-black w-fit p-2 rounded-full rounded-bl-none translate-x-9">Hey!! 👋</span>
-      <h1>🧑‍💻</h1>
-      <h2>Welcome to my blog.</h2>
-      <p>My name is <b class="font-bold">Joaquín Gómez</b>, I'm studying CS, Math and other related areas by myself. <A href="/about">You can learn more about me here.</A></p>
-      <p>Here I'll publish the works made over time, this as well as showing my work to other people is useful as an archive.</p>
-      <p>You can find all types of projects here, related to:</p>
+    <Layout class="gap-14">
+      <div class="flex flex-col gap-2">
+        <h1 class="text-center">Joaquín Gómez</h1>
+        <h4>I'm <b class="font-bold">Joaco</b>, currently studying CS, Math, AI, and other related areas by myself. <A href="/about">You can learn more about me here.</A></h4>
+      </div>
+      <p class="text-sm">Here I'll publish the works made over time, this as well as showing my work to other people is useful as an archive.</p>
+      <div class="flex justify-between items-center flex-wrap gap-5 bg-blue-950 py-16 px-4 rounded-sm">
+        <div class="flex flex-col gap-2">
+          <span class="bg-white text-black w-fit p-2 rounded-full rounded-bl-none translate-x-9 text-lg">Hey! Check out my <A class="text-blue-700" href="/projects">projects</A>.</span>
+          <h1>🧑‍💻</h1>
+        </div>
+        <ul>
+          <li><A class="text-white" href="#">Computer Science & Programming</A></li>
+          <li><A class="text-white" href="#">Web development</A></li>
+          <li><A class="text-white" href="#">AI & Machine Learning</A></li>
+          <li><A class="text-white" href="#">Software Design</A></li>
+          <li><A class="text-white" href="#">Notes & Texts</A></li>
+          <li><A class="text-white" href="#">Mathematics</A></li>
+        </ul>
 
-      <ul>
-        <li>Computer Science & Programming</li>
-        <li>Web development</li>
-        <li>AI & Machine Learning</li>
-        <li>Software Design</li>
-        <li>Notes & Texts</li>
-        <li>Mathematics</li>
-      </ul>
+      </div>
 
-      <p>Note that all of these projects will progress as I do, the purpose of the website is to serve as a checkpoint to see my progress over time. If you are interested you can checkout my <b>Github Account</b>.</p>
-      <A href="https://www.github.com/Joagz">https://www.github.com/Joagz</A>
+      <p>Note that all of these projects will progress as I do, the purpose of the website is to serve as a checkpoint to see my progress over time. If you are interested you can check my social media.</p>
+      <A href="https://www.github.com/Joagz"></A>
     </Layout>
   );
 }
